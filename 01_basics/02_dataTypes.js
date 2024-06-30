a6e23d4a6478cbe83ad9989cb5892b34a6e4f887
akilkhatri104 <84614450+akilkhatri104@users.noreply.github.com>
2024-06-30 06:29:42 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all the JS code as newer version of JS
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "Akil"
+let age = 18
+let isLoggedIn = false
+
+/*
+primitive:
+number => range: 2 to the power 53 
+string => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol => unique
+
+object
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
