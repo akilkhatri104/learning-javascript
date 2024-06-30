de5990e8fc1cdc8740d6add403c82cbe4878d35e
akilkhatri104 <84614450+akilkhatri104@users.noreply.github.com>
2024-06-30 12:26:55 +0000

    conversion of data types

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,49 @@
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+/*
+conversion to number:
+"33" => 33
+"33abc" => NaN
+"Akil" => NaN
+undefined => NaN
+null => 0
+true/false => 1/0
+*/
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+Conversion to Bool
+1/0 => true/false
+"" => false
+"Akil" => true
+null => false
+undefined => false
+*/
+
+let someNumber = undefined
+
+let valueInString = String(someNumber)
+console.log(valueInString);
+console.log(typeof valueInString);
+
+/*
+Conversion to string
+29 => "29"
+true/false => "true"/"false"
+null => null
+undefined => undefined
+
+*/
