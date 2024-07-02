b51e3aedcb82f9ec63e8edd5fed44b43777d714d
akilkhatri104 <84614450+akilkhatri104@users.noreply.github.com>
2024-07-02 12:28:25 +0000

    Data Types Summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,14 @@
+const heroes = ["Ironman","Spiderman","Batman"]
+console.log(typeof heroes)
+
+
+let obj = {
+    name : "Akil",
+    age : 18
+}
+console.log(typeof obj);
+
+const myFun = function(){
+
+}
+console.log(typeof myFun);
