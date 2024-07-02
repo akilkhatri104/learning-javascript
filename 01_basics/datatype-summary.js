a2f71f6f5dfd166aff2402ddb932172757f6611c
akilkhatri104 <84614450+akilkhatri104@users.noreply.github.com>
2024-07-02 12:44:51 +0000

    learnt about Stack and Heap in JS

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -1,14 +1,30 @@
-const heroes = ["Ironman","Spiderman","Batman"]
-console.log(typeof heroes)
+// const heroes = ["Ironman","Spiderman","Batman"]
+// console.log(typeof heroes)
 
 
-let obj = {
-    name : "Akil",
-    age : 18
+// let obj = {
+//     name : "Akil",
+//     age : 18
+// }
+// console.log(typeof obj);
+
+// const myFun = function(){
+
+// }
+// console.log(typeof myFun);
+
+
+//*************Stack Heap***************
+
+let userOne = {
+    email : "user@email.com",
+    upi : "user@ybl"
 }
-console.log(typeof obj);
 
-const myFun = function(){
+let userTwo = userOne
+userTwo.email = "user2@yahoo.com"
 
-}
-console.log(typeof myFun);
+console.log("User 1:");
+console.table(userOne)
+console.log("User 2:");
+console.table(userTwo)
--- 01_basics/tempCodeRunnerFile.js
+++ 01_basics/tempCodeRunnerFile.js
@@ -1,1 +1,14 @@
-54
+eroes = ["Ironman","Spiderman","Batman"]
+// console.log(typeof heroes)
+
+
+// let obj = {
+//     name : "Akil",
+//     age : 18
+// }
+// console.log(typeof obj);
+
+// const myFun = function(){
+
+// }
+// console.log(typeof myFun);
