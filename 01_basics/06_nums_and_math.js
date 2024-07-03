0ad694e726981ae850a4a8e630bd4a8bba26c364
akilkhatri104 <84614450+akilkhatri104@users.noreply.github.com>
2024-07-03 13:34:51 +0000

    Learnt about Numbers and Math in JS

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,21 @@
+// let score = 134
+// console.log(score);
+
+// const bal = new Number(1234.42)
+// console.log(bal);
+// console.log(bal.toPrecision(3));
+
+// const bigValue = 1000000
+// console.log(bigValue.toLocaleString('en-IN'));
+
+//____________________MATH__________________
+
+// console.log(Math.abs(-412));
+// console.log(Math.round(134.13));
+
+console.log(Math.floor((Math.random()*10)+1));
+
+const min = 0
+const max = 6
+
+console.log(Math.floor(Math.random()*(max-min+1)))
