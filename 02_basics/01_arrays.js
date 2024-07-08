bb94cc1a79f5544e00114c5fdcb0d75642cd73e2
akilkhatri104 <84614450+akilkhatri104@users.noreply.github.com>
2024-07-08 14:06:08 +0000

    learnt some Array in JS

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+//arrays
+
+const arr = [1,2,3,4,5]
+const heroes = ["Batman",'Homelander',"Peacemaker"]
+
+
+//array methods
+
+// arr.push(6)
+// console.log(arr);
+
+// const newArr = arr.join()
+// console.log(typeof newArr);
+// console.log(typeof arr);
+
+
+//slice,splice
+const myArr = [0,1,2,3,4,5]
+
+console.log("A ",myArr);
+const myn1 = myArr.slice(2,4)
+console.log(myn1);
+console.log("B ",myArr);
+console.log();
+
+console.log("A ",myArr);
+const myn2 = myArr.splice(2,4)
+console.log(myn2);
+console.log("B ",myArr);
+
+
+
