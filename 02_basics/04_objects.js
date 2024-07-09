e49eccfd5fd43a4c587ef12a1aab42a4248ac3cc
akilkhatri104 <84614450+akilkhatri104@users.noreply.github.com>
2024-07-09 13:40:13 +0000

    some more Objects JS

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,36 @@
+// const appUser = new Object() //- singleton object
+const appUser = {} // - non-singleton object
+
+appUser.id = "123abc"
+appUser.name = "Akil"
+appUser.isLoggedIn = false
+
+// console.log(appUser);
+
+const regularUser = {
+    email:"user@email.com",
+    fullname: {
+        userfullname:{
+            firstname: "Akil",
+            lastname: "Khatri"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign(obj1,obj2)
+const obj3 = {...obj1,...obj2}
+
+// console.log(obj3);
+// console.log(obj1);
+
+console.log(Object.keys(appUser));
+console.log(Object.values(appUser));
+console.log(Object.entries(appUser));
+
+console.log(appUser.hasOwnProperty('isLoggedIn'));
