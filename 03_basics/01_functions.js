0e5623777fc117c365d191178893dd77c5eb3fff
akilkhatri104 <84614450+akilkhatri104@users.noreply.github.com>
2024-07-11 12:30:36 +0000

    Some more function in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -25,4 +25,30 @@ function loginUserMsg(username = "User"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMsg("AKIL"));
+// console.log(loginUserMsg("AKIL"));
+
+function calculateCartPrice(n1,n2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(24,42,54,55));
+
+const user = {
+    username:"Akil",
+    price: 450
+}
+
+function handleObject(anyObject){
+    console.log(`${anyObject.username} has spent ${anyObject.price} rs`);
+}
+
+
+// handleObject(user)
+
+const myArr = [12,4,5,6,76]
+
+function returnThirdValue(arr){
+    return arr[2]
+}
+
+console.log(returnThirdValue(myArr));
