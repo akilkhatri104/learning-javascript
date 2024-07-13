74a04d9fb0a42296f367ac920dc97041b9de15ac
akilkhatri104 <84614450+akilkhatri104@users.noreply.github.com>
2024-07-13 13:45:27 +0000

    Scope level and mini hoisting in JS

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,10 +3,50 @@ if (true) {
     let a = 10
     var c = 30
     const b = 20
-    console.log(a);
+    // console.log(a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one(){
+    const username = "Akil"
+
+    function two(){
+        const website = "YouTube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "Akil"
+    if(true){
+        const website = " YouTube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+//+++++++++++++++ Interesting ++++++++++++++
+
+console.log(addOne(3))
+function addOne(num){
+    return num+1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
+
